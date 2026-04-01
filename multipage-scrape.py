468bdab6117a46520f31c09c6c9d683400e67a48
Mechamorph1903 <danielanorue@gmail.com>
2026-04-01 17:10:58 -0400

    Multipage scrape is me scraping from the smae site but getting all books from 50 pages in the catalogue however first attempt showed me getting 999 books instead of 1000 as stated on the site

---
+++ multipage-scrape.py
@@ -0,0 +1,30 @@
+import requests
+from bs4 import BeautifulSoup
+import urllib3
+urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
+
+
+count = 1
+titles = {}
+while count <= 50:
+    response = requests.get(f"https://books.toscrape.com/catalogue/page-{count}.html", verify=False)
+    response.encoding = "utf-8"
+    soup = BeautifulSoup(response.text, "html.parser")
+    
+    books = soup.find_all("article", class_="product_pod")
+    for book in books:
+        title = book.find("h3").find("a").get("title")
+        price = book.find("div", class_="product_price").find("p", class_="price_color").text
+        # in_stock = book.find("div", class_="product_price").find("p", class_="price_color")
+
+        titles[title] = {
+            "Price": str(price)
+        }
+    count += 1
+        
+
+with open("books.txt", "w", encoding="utf-8") as f:
+    f.write(f"The total Number of Books on the site is {len(titles)}\n\n\n")
+    for title, values in titles.items():
+        f.write(f"{title}: {values['Price']}\n")   # \n is a new line
+
